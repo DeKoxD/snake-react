58e35184416ba9210f0ab095ec7782104a303add
André Pantaleão <22629109+DeKoxD@users.noreply.github.com>
2024-05-15 22:35:12 +0100

    fix: broken unused stuff

--- src/style/DisplayCellSnakeBody.tsx
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-import DisplayCell from "./DisplayCell";
-import { DisplayCellProps } from "./DisplayCell";
-
-const DisplayCellSnakeBody = styled(DisplayCell)<DisplayCellProps>`
-  background-color: ${(props) => props.theme.colors.snakeBody};
-`;
-
-export default DisplayCellSnakeBody;
--- src/style/DisplayRow.tsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-const DisplayRow = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
-
-export default DisplayRow;
